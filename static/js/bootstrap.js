e17613e7eecc284cc7aa64fbb046de86191a046e
Sarthak Lamba <samlamba29@gmail.com>
2022-11-08 16:28:20 +0530

    Final Updates

--- static/js/bootstrap.js
+++
@@ -1,5 +0,0 @@
-function copyPass() {
-    const cb = navigator.clipboard;
-    const password = document.querySelector('h2');
-    cb.writeText(password.innerText).then(() => alert('Text copied'));
-  }
